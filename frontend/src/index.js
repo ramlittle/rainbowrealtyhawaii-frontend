75c4271adfd3cb1b3cbd3aa99085891c6c437f69
ramelitomartinez <ramelitomartinez@gmail.com>
2023-02-28 14:27:03 -0500

    feature made router work

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+//ROUTING
+import {BrowserRouter} from 'react-router-dom';
+//REDUX
 import store from './redux/store';
 import {Provider} from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
